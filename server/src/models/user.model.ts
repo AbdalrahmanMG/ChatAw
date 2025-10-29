d4fdf28192c0ef4c888636c89fd3af74e72e6124
AbdalrahmanMG <abdalrahman.m.gomaa@gmail.com>
2025-10-29 14:15:14 +0300

    feat(server-model): create user model

---
+++ server/src/models/user.model.ts
@@ -0,0 +1,54 @@
+import mongoose, { Schema } from "mongoose";
+import { compareValue, hashValue } from "../utils/bcrypt";
+
+export interface IUser {
+  name: string;
+  email: string;
+  password: string;
+  avatar?: string | null;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    name: { type: String },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar: { type: String, default: null },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform: (doc, ret) => {
+        if (ret) {
+          delete (ret as any).password;
+        }
+        return ret;
+      },
+    },
+  }
+);
+
+
+userSchema.pre("save", async function(next) {
+  if (this.password && this.isModified("password")){
+    this.password = await hashValue(this.password)
+  }
+})
+
+userSchema.methods.comparePassword = async function( val: string) {
+  return compareValue(val, this.password)
+}
+
+const UserModel = mongoose.model<IUser>("User", userSchema)
+export default UserModel
