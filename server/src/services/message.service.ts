6e1c68c1396579d2ef9241e19f5f4e33f57e2bc6
AbdalrahmanMG <abdalrahman.m.gomaa@gmail.com>
2025-11-02 20:02:21 +0200

    feat(server-upload): config cloudinary to upload images and use in messages

--- server/src/services/message.service.ts
+++ server/src/services/message.service.ts
@@ -1,3 +1,4 @@
+import cloudinary from "../config/cloudinary.config";
 import chatModel from "../models/chat.model";
 import messageModel from "../models/message.model";
 import { BadRequestException, NotFoundException } from "../utils/appError";
@@ -8,10 +9,10 @@ export const sendMessageService = async (
     chatId: string;
     content?: string;
     image?: string;
-    replyToId?: string;
+    replyTo?: string;
   }
 ) => {
-  const { chatId, content, image, replyToId } = body;
+  const { chatId, content, image, replyTo: replyToId } = body;
 
   const chat = await chatModel.findOne({
     _id: chatId,
@@ -28,34 +29,33 @@ export const sendMessageService = async (
       chatId,
     });
     if (!replyMessage) throw new NotFoundException("Reply Message not found");
-
-    let imageUrl;
-
-    if(image) {
-      // upload image
-    }
-
-    const newMessage = await messageModel.create({
-      chatId,
-      sender: userId,
-      content,
-      image: imageUrl,
-      replyTo: replyToId || null
-    })
-
-    await newMessage.populate([
-      {path:" sender", select: "name avatar"},
-      {path: "replyTo",
-        select: "content image sender",
-        populate: {
-          path: "sender",
-          select: "name avatar"
-        }
-      }
-    ])
-
-
-
-    return {message: newMessage, chat}
   }
+  let imageUrl;
+
+  if (image) {
+    const uploadRes = await cloudinary.uploader.upload(image);
+    imageUrl = uploadRes.secure_url;
+  }
+
+  const newMessage = await messageModel.create({
+    chatId,
+    sender: userId,
+    content,
+    image: imageUrl,
+    replyTo: replyToId || null,
+  });
+
+  await newMessage.populate([
+    { path: "sender", select: "name avatar" },
+    {
+      path: "replyTo",
+      select: "content image sender",
+      populate: {
+        path: "sender",
+        select: "name avatar",
+      },
+    },
+  ]);
+
+  return { userMessage: newMessage, chat };
 };
