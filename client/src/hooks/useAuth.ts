9d21a560ef0b2db569f5631f5eece01b3b4747a3
AbdalrahmanMG <abdalrahman.m.gomaa@gmail.com>
2025-11-04 22:53:51 +0200

    feat(client-auth): setup authentication hook and socket hook for connection and disconnection

---
+++ client/src/hooks/useAuth.ts
@@ -0,0 +1,82 @@
+import type { LoginType, RegisterType, UserType } from "@/types/authType";
+import { persist } from "zustand/middleware";
+import { create } from "zustand";
+import { API } from "@/lib/axios";
+import { toast } from "sonner";
+import { useSocket } from "./useSocket";
+
+interface IAuthState {
+  user: UserType | null;
+  isLoggingIn: boolean;
+  isSigningup: boolean;
+  isAuthStatusLoading: boolean;
+
+  register: (data: RegisterType) => void;
+  login: (data: LoginType) => void;
+  logout: () => void;
+  isAuthStatus: () => void;
+}
+
+export const useAuth = create<IAuthState>()(
+  persist(
+    (set) => ({
+      user: null,
+      isLoggingIn: false,
+      isSigningup: false,
+      isAuthStatusLoading: false,
+
+      register: async (data: RegisterType) => {
+        set({ isSigningup: true });
+        try {
+          const res = await API.post("/auth/register", data);
+          set({ user: res.data.user });
+          useSocket.getState().connectSocket();
+        } catch (error: Error | unknown) {
+          const err = error as { response?: { data?: { message?: string } } };
+          toast.error(err.response?.data?.message || "Register failed");
+        } finally {
+          set({ isSigningup: false });
+        }
+      },
+      login: async (data: LoginType) => {
+        set({ isLoggingIn: true });
+        try {
+          const res = await API.post("/auth/login", data);
+          set({ user: res.data.user });
+          useSocket.getState().connectSocket();
+        } catch (error: Error | unknown) {
+          const err = error as { response?: { data?: { message?: string } } };
+          toast.error(err.response?.data?.message || "Login failed");
+        } finally {
+          set({ isLoggingIn: false });
+        }
+      },
+      logout: async () => {
+        try {
+          await API.post("/auth/logout")
+          set({user: null})
+          useSocket.getState().disconnectSocket()  
+        } catch (error: Error | unknown) {
+          const err = error as { response?: { data?: { message?: string } } };
+          toast.error(err.response?.data?.message || "Login failed");
+        }
+      },
+      isAuthStatus: async () => {
+         set({ isAuthStatusLoading: true });
+        try {
+          const res = await API.post("/auth/status");
+          set({ user: res.data.user });
+          useSocket.getState().connectSocket();
+        } catch (error: Error | unknown) {
+          const err = error as { response?: { data?: { message?: string } } };
+          toast.error(err.response?.data?.message || "Authentication failed");
+        } finally {
+          set({ isAuthStatusLoading: false });
+        }
+      },
+    }),
+    {
+      name: "root-auth",
+    }
+  )
+);
---
+++ client/src/types/authType.ts
@@ -0,0 +1,20 @@
+export type LoginType = {
+    email: string;
+    password: string;
+}
+
+export type RegisterType = {
+    name: string;
+    email: string;
+    password: string;
+    avatar?: string | undefined;
+}
+
+export type UserType = {
+  _id: string
+  name: string;
+  email: string;
+  avatar?: string | null;
+  createdAt: Date;
+  updatedAt: Date;
+}
